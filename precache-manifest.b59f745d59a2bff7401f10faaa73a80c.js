4f7f8fad31516ccc6c3102c692487f9df7e849d3
unknown <sayak649@gmail.com>
2020-09-13 01:18:06 +0530

    Updates

---
+++ precache-manifest.b59f745d59a2bff7401f10faaa73a80c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37a6a95f8a8a09ffb06308b89179586",
+    "url": "/ghoshFolio/index.html"
+  },
+  {
+    "revision": "5610cc786df30223126b",
+    "url": "/ghoshFolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6ad1e2163d36211c708a",
+    "url": "/ghoshFolio/static/js/2.0fcc8e18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghoshFolio/static/js/2.0fcc8e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5610cc786df30223126b",
+    "url": "/ghoshFolio/static/js/main.9884f12e.chunk.js"
+  },
+  {
+    "revision": "2f32a6dcbf25b8f65e66",
+    "url": "/ghoshFolio/static/js/runtime-main.e83942ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghoshFolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
